e5d4bec337050b2e04a2950fbb139085b979c877
rivera191 <cristianrivera096@gmail.com>
2019-02-18 15:03:05 -0500

    Initial commit

---
+++ PersonalityQuiz/IntroductonViewController.swift
@@ -0,0 +1,23 @@
+//
+//  IntroductionViewController.swift
+//  PersonalityQuiz
+//
+//  Created by Cristian Rivera on 2/12/18.
+//  Copyright © 2019 Cristian Rivera. All rights reserved.
+//
+
+import UIKit;
+
+class IntroductonViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad();
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    //Called when the ResultsViewController's Done button is pressed.
+
+    @IBAction func unwindToQuizIntroduction(segue: UIStoryboardSegue) {
+    }
+}
+
